d51bd35e2f1efbe1c7514e97098158f2758879cd
shanlihou <shanlihou@gmail.com>
2018-08-23 19:09:34 +0800

    first blood

---
+++ tunnel.py
@@ -0,0 +1,33 @@
+from ftplib import FTP
+import os
+
+
+class Tunnel(object):
+    def __init__(self, ip, port=None):
+        self.ip = ip
+        self.port = port
+        ftp = FTP()
+        ftp.connect(self.ip)
+        ftp.login('123')
+        self.ftp = ftp
+
+    def fileInfoCB(self, fileInfo):
+        infoList = fileInfo.split()
+        fileName = infoList[-1]
+        if not fileName.endswith('.py'):
+            return
+
+        with open(fileName, 'wb') as fw:
+            #downName = 'RETR ' + os.path.join(self.downPath, fileName)
+            downName = 'RETR ' + fileName
+            self.ftp.retrbinary(downName, fw)
+
+    def test(self):
+        self.downPath = r'python\pythonFunc\theirFTP'
+        self.ftp.cwd(self.downPath)
+        self.ftp.dir(self.fileInfoCB)
+
+
+if __name__ == '__main__':
+    tunnel = Tunnel('192.168.16.67')
+    tunnel.test()
